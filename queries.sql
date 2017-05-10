1e71b29326d7823eed2a683dab60ac14bb0e3354
Julian Poyourow <julianpoyo@gmail.com>
2017-05-10 06:44:17 -0700

    Fuck late nights and early mornings. I wanna sleep

--- queries.sql
+++ queries.sql
@@ -43,7 +43,7 @@ SELECT * FROM mechanicSkills
 
 -- 6
 SELECT cost AS packageCost, itemName, milage, make, model, year FROM package
-    LEFT JOIN packageItem USING (milage, make, model, year)
+    LEFT JOIN packageItem USING (milage, make, model, year);
 
 -- 7 DIDNT DO
 
@@ -54,23 +54,38 @@ SELECT customerName, totalLoyaltyPoints - spent AS loyaltyPointsRemaining FROM s
     INNER JOIN (
         SELECT customerID, SUM(numberLoyaltyPointsSpent) AS spent FROM loyaltyPointsSpent
         GROUP BY customerID
-    ) a
+    ) a ON a.customerID = customer.customerID
+    ORDER BY loyaltyPointsRemaining DESC;
 
--- 9 DIDNT DO
-
+-- 9
+SELECT customerName, income, premierServiceValue, income - premierServiceValue AS netProfit FROM customer
+    INNER JOIN premier USING (customerID)
+    INNER JOIN (
+        SELECT customerID, SUM(cost) AS income FROM scheduledMaintenance
+            WHERE DATEDIFF(CURDATE(), dateTime) < 365
+            GROUP BY customerID
+    ) a ON premier.customerID = a.customerID
+    ORDER BY netProfit DESC;
 
 -- 10
-
-
--- 11
 SELECT customerName, netProfit FROM customer
     INNER JOIN steady USING (customerID)
     INNER JOIN (
         SELECT customerID, SUM(cost) AS netProfit FROM scheduledMaintenance
             WHERE DATEDIFF(CURDATE(), dateTime) < 365
             GROUP BY customerID
-    ) a
+    ) a ON steady.customerID = a.customerID
     ORDER BY netProfit DESC;
+
+-- 11
+SELECT customerName, income FROM customer
+    INNER JOIN premier USING (customerID)
+    INNER JOIN (
+        SELECT customerID, SUM(cost) AS income FROM scheduledMaintenance
+            WHERE DATEDIFF(CURDATE(), dateTime) < 365
+            GROUP BY customerID
+    ) a ON premier.customerID = a.customerID
+    ORDER BY income DESC;
 
 -- 12
 SELECT * FROM instanceOfCar
@@ -90,5 +105,32 @@ SELECT * FROM mentoringRelationship
         HAVING count(skillName) = (SELECT MAX(skillCount) FROM (
             SELECT mentorID, count(skillName) AS skillCount FROM mentoringRelationship
                 GROUP BY mentorID) a))
+
+-- 14
+SELECT skillName, count(employeeID) AS employeeCount FROM mechanicSkills
+    GROUP BY skillName
+    ORDER BY employeeCount ASC
+    LIMIT 3;
+
+-- 15
+SELECT * FROM employee
+    INNER JOIN serviceTechnician USING (employeeID)
+    INNER JOIN mechanic USING (employeeID)
+
+-- 16
+-- #1 CHECK OUR PRICES COMPARED TO THE COMPETITOR. FIND OUR LARGEST PRICE DIFFERENCES (IN A BAD WAY)
+SELECT itemName, competitorService.cost AS theirCost, item.cost AS ourCost, competitorService.cost - item.cost AS difference FROM competitorService
+    INNER JOIN item USING (itemName)
+    INNER JOIN competitor USING (competitorID)
+    ORDER BY difference ASC
+
+-- #2
+SELECT * FROM competitorService
+    INNER JOIN item USING (itemName)
+    INNER JOIN competitor USING (competitorID)
+    WHERE itemName LIKE '%Front Window%'
+
+-- #3
+
 
 

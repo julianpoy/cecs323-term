a6cfdbb155125fe95976ad80be5cc7b7f724da2d
jblacklock <jessebblacklock@gmail.com>
2017-05-09 19:59:24 -0700

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,17 +1,17 @@
 INSERT INTO customer(customerID, email, phone, customerName, dateJoined) VALUES 
-(1,'mindcontrol.maniac@marvel.com',9999998888,'Jessica Jones', 2006-11-03),
-(2,'not.blind@marvel.com',9999997777,'Matthew Murdock',2004-05-11),
-(3,'horrible.acting@marvel.com',9999996666,'Danny Rand',2010-12-10),
-(4,'falcon.punch@marvel.com',9999995555,'Luke Cage', 1993-11-06),
-(5,'captamerica.sucks@marvel.com',9999994444,'Tony Stark', 2013-05-07),
-(6,'MURICAAAA@marvel.com',9999993333,'Steve Rogers', 1999-03-13),
-(7,'mini.ironman@marvel.com',9999992222,'c O Grady', 2001-08-19),
-(8,'i.need.my.own.movie@marvel.com',9999991111,'Natasha Romanoff', 2007-09-15),
-(9,'mindcontrol.master@marvel.com',9999990000,'Wanda Maximoff', 1998-08-23),
-(10,'remake.my.movie.again@marvel.com',9991110000,'Peter Parker', 1997-10-30),
-(11,'my.arms.break@marvel.com',7779990000,'Daisy Johnson', 2005-01-01),
-(12,'iseedeadpeople@marvel.com',9999990000,'Phill Coulson', 2002-07-07),
-(13,'ihavebrotherissues@marvel.com',9999990000,'Grant Ward', 2005-04-15);
+(1,'mindcontrol.maniac@marvel.com',9999998888,'Jessica Jones', '2006-11-03'),
+(2,'not.blind@marvel.com',9999997777,'Matthew Murdock','2004-05-11'),
+(3,'horrible.acting@marvel.com',9999996666,'Danny Rand','2010-12-10'),
+(4,'falcon.punch@marvel.com',9999995555,'Luke Cage', '1993-11-06'),
+(5,'captamerica.sucks@marvel.com',9999994444,'Tony Stark', '2013-05-07'),
+(6,'MURICAAAA@marvel.com',9999993333,'Steve Rogers', '1999-03-13'),
+(7,'mini.ironman@marvel.com',9999992222,'c O Grady', '2001-08-19'),
+(8,'i.need.my.own.movie@marvel.com',9999991111,'Natasha Romanoff', '2007-09-15'),
+(9,'mindcontrol.master@marvel.com',9999990000,'Wanda Maximoff', '1998-08-23'),
+(10,'remake.my.movie.again@marvel.com',9991110000,'Peter Parker', '1997-10-30'),
+(11,'my.arms.break@marvel.com',7779990000,'Daisy Johnson', '2005-01-01'),
+(12,'iseedeadpeople@marvel.com',9999990000,'Phill Coulson', '2002-07-07'),
+(13,'ihavebrotherissues@marvel.com',9999990000,'Grant Ward', '2005-04-15');
 
 
 INSERT INTO employee(name, phone, jobTitle) VALUES 

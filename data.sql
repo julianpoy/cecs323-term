cb5dd7580cdb8b63a4d724be1680ce5a00af7353
jblacklock <jessebblacklock@gmail.com>
2017-05-09 19:28:09 -0700

    Update data.sql

---
+++ data.sql
@@ -0,0 +1,14 @@
+INSERT INTO customers(customerID, email, phone, customerName) VALUES 
+(1,'mindcontrol.maniac@marvel.com',9999998888,'Jessica Jones'),
+(2,'not.blind@marvel.com',9999997777,'Matthew Murdock'),
+(3,'horrible.acting@marvel.com',9999996666,'Danny Rand'),
+(4,'falcon.punch@marvel.com',9999995555,'Luke Cage'),
+(5,'captamerica.sucks@marvel.com',9999994444,'Tony Stark'),
+(6,'MURICAAAA@marvel.com',9999993333,'Steve Rogers'),
+(7,'mini.ironman@marvel.com',9999992222,1,1,'c O Grady'),
+(8,'i.need.my.own.movie@marvel.com',9999991111,'Natasha Romanoff'),
+(9,'mindcontrol.master@marvel.com',9999990000,'Wanda Maximoff'),
+(10,'remake.my.movie.again@marvel.com',9991110000,'Peter Parker'),
+(11,'my.arms.break@marvel.com',7779990000,'Daisy Johnson'),
+(12,'iseedeadpeople@marvel.com',9999990000,'Phill Coulson'),
+(13,'ihavebrotherissues@marvel.com',9999990000,'Grant Ward');

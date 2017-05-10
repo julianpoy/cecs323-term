19c286ed8843a00359a8ac32c7633ad3c504fc53
jblacklock <jessebblacklock@gmail.com>
2017-05-09 19:42:50 -0700

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,11 +1,11 @@
-INSERT INTO customer(customerID, email, phone, customerName) VALUES 
+INSERT INTO customers(customerID, email, phone, customerName) VALUES 
 (1,'mindcontrol.maniac@marvel.com',9999998888,'Jessica Jones'),
 (2,'not.blind@marvel.com',9999997777,'Matthew Murdock'),
 (3,'horrible.acting@marvel.com',9999996666,'Danny Rand'),
 (4,'falcon.punch@marvel.com',9999995555,'Luke Cage'),
 (5,'captamerica.sucks@marvel.com',9999994444,'Tony Stark'),
 (6,'MURICAAAA@marvel.com',9999993333,'Steve Rogers'),
-(7,'mini.ironman@marvel.com',9999992222,'c O Grady'),
+(7,'mini.ironman@marvel.com',9999992222,1,1,'c O Grady'),
 (8,'i.need.my.own.movie@marvel.com',9999991111,'Natasha Romanoff'),
 (9,'mindcontrol.master@marvel.com',9999990000,'Wanda Maximoff'),
 (10,'remake.my.movie.again@marvel.com',9991110000,'Peter Parker'),
@@ -14,13 +14,13 @@ INSERT INTO customer(customerID, email, phone, customerName) VALUES
 (13,'ihavebrotherissues@marvel.com',9999990000,'Grant Ward');
 
 
-INSERT INTO employee(name, phone, jobTitle) 
-VALUES ('Bruce Wayne', 6088811111, 'Service Technician'),
-VALUES ('Clark Kent', 6467771111, 'Mechanic'),
-VALUES ('Selina Kyle', 6086662222, 'Intern'),
-VALUES ('Jason Todd', 6085553333, 'Intern'),
-VALUES ('James Gordon', 6084443333, 'Mechanic'),
-VALUES ('Harvey Dent', 6083334444, 'Mechanic'),
-VALUES ('Barry Allen', 6082225555, 'Service Technician'),
-VALUES ('Hal Jordan', 6081116666, 'Mechanic'),
-VALUES ('Oliver Queen', 6080007777, 'Intern');
+INSERT INTO employee(name, phone, jobTitle) VALUES 
+('Bruce Wayne', 6088811111, 'Service Technician'),
+('Clark Kent', 6467771111, 'Mechanic'),
+('Selina Kyle', 6086662222, 'Intern'),
+('Jason Todd', 6085553333, 'Intern'),
+('James Gordon', 6084443333, 'Mechanic'),
+('Harvey Dent', 6083334444, 'Mechanic'),
+('Barry Allen', 6082225555, 'Service Technician'),
+('Hal Jordan', 6081116666, 'Mechanic'),
+('Oliver Queen', 6080007777, 'Intern');

7eaf411301c1aa18b41d1e6d0ab7a459120d8104
Julian Poyourow <julianpoy@users.noreply.github.com>
2017-05-09 19:41:32 -0700

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,11 +1,11 @@
-INSERT INTO customers(customerID, email, phone, customerName) VALUES 
+INSERT INTO customer(customerID, email, phone, customerName) VALUES 
 (1,'mindcontrol.maniac@marvel.com',9999998888,'Jessica Jones'),
 (2,'not.blind@marvel.com',9999997777,'Matthew Murdock'),
 (3,'horrible.acting@marvel.com',9999996666,'Danny Rand'),
 (4,'falcon.punch@marvel.com',9999995555,'Luke Cage'),
 (5,'captamerica.sucks@marvel.com',9999994444,'Tony Stark'),
 (6,'MURICAAAA@marvel.com',9999993333,'Steve Rogers'),
-(7,'mini.ironman@marvel.com',9999992222,1,1,'c O Grady'),
+(7,'mini.ironman@marvel.com',9999992222,'c O Grady'),
 (8,'i.need.my.own.movie@marvel.com',9999991111,'Natasha Romanoff'),
 (9,'mindcontrol.master@marvel.com',9999990000,'Wanda Maximoff'),
 (10,'remake.my.movie.again@marvel.com',9991110000,'Peter Parker'),

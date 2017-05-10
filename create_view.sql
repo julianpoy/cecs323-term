26451aebbfa2a8ebf5ead3172983abc393edd678
Julian Poyourow <julianpoyo@gmail.com>
2017-05-09 20:27:45 -0700

    Added customer addresses view

--- create_view.sql
+++ create_view.sql
@@ -8,3 +8,21 @@ SELECT customerName,
     LEFT JOIN premier USING (customerID)
     LEFT JOIN steady USING (customerID)
     LEFT JOIN prospective USING (customerID);
+
+CREATE VIEW Customer_addresses_v AS
+SELECT customerName,
+    dateJoined,
+    corporation.customerID IS NOT NULL AS isCorporate,
+    private.address AS privateAddress,
+    private.city AS privateCity,
+    private.state AS privateState,
+    private.zip AS privateZip,
+    address.address AS corporateAddress,
+    address.city AS corporateCity,
+    address.state AS corporateState,
+    address.zip AS corporateZip
+    FROM customer
+    LEFT JOIN corporation USING (customerID)
+    LEFT JOIN address USING (customerID)
+    LEFT JOIN private USING (customerID);
+
